e69bfb66d22b53d1f61e44d36f630c03f9e816da
Igor Sakhankov <igor.sakhankov@glovoapp.com>
2023-02-10 14:01:49 +0100

    fix: broken test

---
+++ springboot/src/test/java/com/harbour/softwareengineering/springboot/restfulapp/ToDoControllerTest.java
@@ -0,0 +1,18 @@
+package com.harbour.softwareengineering.springboot.restfulapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+class ToDoControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void myTest() {
+        return;
+    }
+}
