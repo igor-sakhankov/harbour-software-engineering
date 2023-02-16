ab88f4bb400265ef3381f9fed53d9fad8195643d
Igor Sakhankov <igor.sakhankov@glovoapp.com>
2023-02-16 20:27:56 +0100

    chore(gh packages): use the name from gh

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/igor-sakhankov/harbour-software-engineering")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
